c6fdd6792ee391bae391d758bfb90e07337d8ac2
Unknown <wndtanaka@gmail.com>
2017-08-15 14:31:05 +1000

    Pinball mechanics

---
+++ Assets/Scripts/BallBrain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBrain : MonoBehaviour
+{
+    Rigidbody rigid;
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "bounce")
+        {
+            print("Hello");
+            rigid.velocity = ((transform.position - collision.contacts[0].point) * 50);
+        }
+    }
+}
